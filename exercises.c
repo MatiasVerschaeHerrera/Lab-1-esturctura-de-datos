30e705cab0bc4e26b14326293c3b241fcad8f4c7
Your Name <you@example.com>
2026-03-22 17:19:18 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -119,7 +119,9 @@ int parentesisBalanceados(char *cadena) {
    while(cadena[posCadena] != '\0'){
       char elemento = cadena[posCadena];
       if(elemento == '(' || elemento == '{' || elemento == '['){
-         push(pila, &elemento);
+         char* nuevoCaracter = (char*)malloc(sizeof(char));
+         *nuevoCaracter = elemento;
+         push(pila, nuevoCaracter);
       }
       else if(elemento == ')' || elemento == '}' || elemento == ']'){
          if(top(pila) == NULL) return 0;
